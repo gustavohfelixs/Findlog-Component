5bfc01a6c6ebf799c09ecb09a629ed8b62cefdc4
Gustavo Félix <99498636+gustavohfelixs@users.noreply.github.com>
2023-11-22 19:05:12 +0000

    feat - first commit

---
+++ base/src/main/java/com/findlog/base/controller/UsuarioController.java
@@ -0,0 +1,36 @@
+package com.findlog.base.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.findlog.base.domain.Usuario;
+import com.findlog.base.repository.UsuarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UsuarioController {
+    
+    private final UsuarioRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    
+    @PostMapping("/register")
+    public ResponseEntity<String> findUser(@RequestBody Usuario user) {
+        
+        if(!repository.findByEmail(user.getEmail()).isEmpty()) return ResponseEntity.badRequest().build();
+
+        var encondedPassword = passwordEncoder.encode(user.getSenha());
+
+        user.setSenha(encondedPassword);
+
+        repository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+    
+}
